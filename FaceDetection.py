e3abd3f09cdafbc0f967c2f0802f1310aa96b75f
Kunal.P.Sangurmath <sangurmathkunal@gmail.com>
2020-09-09 17:49:36 +0530

    Create FaceDetection.py

---
+++ FaceDetection.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = plt.imread("Image.png") #You can also use JPEG file
+plt.imshow(img)
+img.shape
+
+import cv2 # Download command for OpenCV package : pip install -c opencv-python
+model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') #Xml file download link is given in Readme
+faces = model.detectMultiScale(img)
+for face in faces:
+    x,y,w,h = face
+    im = cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,0), 2)
+    
+plt.imshow(im)
